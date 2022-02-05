3f34fee1767148730a283708e01db5bf2b4d7923
knight <knight.sarai.dev@gmail.com>
2022-02-05 23:56:30 +0200

    Add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,30 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
+import { WorkoutsComponent } from './workouts/workouts.component';
+import { WorkoutListComponent } from './workouts/workout-list/workout-list.component';
+import { WorkoutDetailComponent } from './workouts/workout-detail/workout-detail.component';
+import { WorkoutComponent } from './workouts/workout-list/workout/workout.component';
+import { EquipmentsComponent } from './equipments/equipments.component';
+import { EquipmentEditComponent } from './equipments/equipment-edit/equipment-edit.component';
+import {MatGridListModule} from "@angular/material/grid-list";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    WorkoutsComponent,
+    WorkoutListComponent,
+    WorkoutDetailComponent,
+    WorkoutComponent,
+    EquipmentsComponent,
+    EquipmentEditComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
