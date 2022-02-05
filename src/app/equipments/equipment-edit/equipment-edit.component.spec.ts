c48ba4f7c17924d4988a5aae251e47725ef5e7dc
knight <knight.sarai.dev@gmail.com>
2022-02-05 23:11:28 +0200

    Init app components

---
+++ src/app/equipments/equipment-edit/equipment-edit.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EquipmentEditComponent } from './equipment-edit.component';
+
+describe('EquipmentEditComponent', () => {
+  let component: EquipmentEditComponent;
+  let fixture: ComponentFixture<EquipmentEditComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EquipmentEditComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EquipmentEditComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
