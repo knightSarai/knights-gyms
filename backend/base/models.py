ce888b986bd00979d69bf13b3e02e57fe0a9b694
knight <afayyad@celtisaustralis.com>
2022-06-21 00:34:18 +0300

    Add logical delete on the BaseModel Make the name field unique

--- backend/base/models.py
+++ backend/base/models.py
@@ -6,9 +6,13 @@ class BaseModel(models.Model):
     updated_at = models.DateTimeField('Updated at', auto_now=True)
     active = models.BooleanField('Active', default=True)
 
+    def delete(self, *args, **kwargs):
+        self.active = False
+        self.save()
+
 
 class MasterModel(BaseModel):
-    name = models.CharField('Name', max_length=255, null=False)
+    name = models.CharField('Name', unique=True, max_length=255, null=False)
 
     def __str__(self):
         return self.name
