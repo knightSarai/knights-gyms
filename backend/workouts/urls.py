075d82d227000e12d19da8b78c65bc3496845d38
knight <afayyad@celtisaustralis.com>
2022-06-21 00:49:16 +0300

    Override workout equipment when update the workout

--- backend/workouts/urls.py
+++ backend/workouts/urls.py
@@ -7,4 +7,5 @@ app_name = "workouts"
 
 urlpatterns = [
     path('', views.WorkoutListView.as_view(), name='all_workouts'),
+    path('<int:pk>/', views.WorkoutDetailView.as_view(), name='workout_detail'),
 ]
--- backend/workouts/views.py
+++ backend/workouts/views.py
@@ -1,10 +1,12 @@
 from django.db import transaction
+from django.http import JsonResponse
 from rest_framework import generics, status
 from rest_framework.response import Response
 
 from equipments.models import Equipment
 from equipments.schemas import WorkoutEquipmentSchema
 from .models import Workout, WorkoutEquipment
+from .schemas import WorkoutSchema
 from .serializers import WorkoutSerializer
 
 
@@ -15,6 +17,7 @@ class WorkoutListView(generics.ListCreateAPIView):
     def post(self, request, *args, **kwargs):
         post_data = request.data
         equipments_json = post_data.get('equipments') or []
+
         equipment_schemas = [WorkoutEquipmentSchema(**equipment) for equipment in equipments_json]
 
         with transaction.atomic():
@@ -44,3 +47,42 @@ class WorkoutListView(generics.ListCreateAPIView):
         headers = self.get_success_headers(serializer.data)
 
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class WorkoutDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Workout.objects.filter(active=True)
+    serializer_class = WorkoutSerializer
+
+    def put(self, request, *args, **kwargs):
+        workout_id = kwargs.get('pk')
+        put_data = request.data
+        workout_schema = WorkoutSchema(**put_data)
+        equipments_json = put_data.get('equipments') or []
+
+        equipment_schemas = [WorkoutEquipmentSchema(**equipment) for equipment in equipments_json]
+
+        equipments = [
+            {
+                "object": Equipment.objects.get_or_create(
+                    **equipment_schema.dict(exclude_unset=True, exclude={'amount'})
+                )[0],
+                "amount": equipment_schema.amount
+            }
+            for equipment_schema in equipment_schemas
+        ]
+
+        workout = Workout.objects.get(id=workout_id)
+        with transaction.atomic():
+            workout.update(workout_schema.dict())
+            WorkoutEquipment.objects.filter(workout=workout).delete()
+
+            for equipment in equipments:
+                WorkoutEquipment.objects.create(
+                    workout=workout,
+                    equipment=equipment['object'],
+                    amount=equipment['amount']
+                )
+
+        return JsonResponse({'message': 'Workout updated'}, status=status.HTTP_200_OK)
+
+
